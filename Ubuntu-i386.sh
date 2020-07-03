0641d968446ea5129bfc2e2df1880e3a7829f204
AkiraYuki <44525248+AllPlatform@users.noreply.github.com>
2020-07-03 10:01:20 +0700

    Update Ubuntu-i386.sh

--- Ubuntu-i386.sh
+++ Ubuntu-i386.sh
@@ -58,7 +58,7 @@ command+=" -0"
 command+=" -r $folder -q qemu-i386-static"
 command+=" -b /dev"
 command+=" -b /proc"
-command+=" -b debian-fs/root:/dev/shm"
+command+=" -b ubuntu-fs/root:/dev/shm"
 ## uncomment the following line to have access to the home directory of termux
 #command+=" -b /data/data/com.termux/files/home:/root"
 ## uncomment the following line to mount /sdcard directly to / 
